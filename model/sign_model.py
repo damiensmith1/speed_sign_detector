5f76bc808640ccdbc3489933a50167f6de8afdb6
damiensmith1 <damiensmith@damiens-air.home>
2024-01-18 17:00:51 -0500

    refactor skeletons

---
+++ model/sign_model.py
@@ -0,0 +1,9 @@
+
+import torch.nn as nn
+
+class SpeedSignIdentification(nn.Module):
+    def __init__(self):
+        super(SpeedSignIdentification, self).__init__()
+
+    def forward(X):
+        return X
---
+++ model/speed_limit_dataset.py
@@ -0,0 +1,18 @@
+
+from torch.utils.data import Dataset
+
+class SpeedLimitDataset(Dataset):
+    def __init__(self, data_dir, label_path, transform=None):
+        self.data_dir = data_dir
+        self.transform = transform
+
+        self.labels_file = label_path
+        self.image_paths = []
+        self.speed_label = []
+        
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        return idx
+
